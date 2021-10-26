2e5af1535ba334d137a44de0a5a8e506a8ac5257
azimny86 <azimny86@gmail.com>
2021-10-26 15:08:59 +0000

    Added a search functionality and no image picture

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,9 +8,21 @@ def all_products(request):
     """ View to show up all products  """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'You did not enter anything to search')
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter((queries))
 
     context = {
         'products': products,
+        'search_term':  query,
     }
 
     return render(request, 'products/products.html', context)
